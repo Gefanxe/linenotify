4d32f1f8c60ed3a66b877143cadb0484dfd2c1d7
Andersen <actfire@gmail.com>
2020-09-28 02:00:23 +0800

    update

--- routes/linenotify.js
+++ routes/linenotify.js
@@ -9,6 +9,44 @@ const client_id = 'Gf1nOjPTGZ5bdBT0IyO5QJ';
 const client_secret = 'xl9HcD1FQKyKCqs1U3EK6ggBRLS87qPh4EvUu0t3cmn';
 const redirect_uri = 'http://localhost:3000/line/callback';
 
+// TODO: 綁 NFC卡片頁面
+
+// TODO: make form to save token and kids's name and their parents name
+router.post('/line/callback', function (req, res) {
+    // console.dir(req.body);
+    const code = req.body.code;
+    getAccessToken(code, function (response) {
+        if (response.status === 200) {
+            // console.log('ok: ' + response.data.access_token);
+            res.send('ok: ' + response.data.access_token);
+        } else {
+            // console.log(response);
+            res.send('fail... ');
+        }
+    });
+});
+
+// 註冊及測試
+router.get('/line/regist', function (req, res) {
+    res.render('lineregist', {
+        title: '首頁',
+        client_id: client_id,
+        ipport: ip.address() + ':3000',
+        redirect_uri: redirect_uri
+    });
+});
+
+router.post('/line/sendmsg', function (req, res) {
+    // console.dir(req.body);
+    const token = req.body.token;
+    const msg = req.body.msg;
+    sendMsg(token, msg, function(response) {
+        // TODO: fix
+        res.json();
+    });
+});
+
+
 function getAccessToken(code, cb) {
     let formdata = new FormData();
 
@@ -49,38 +87,5 @@ function sendMsg(token, msg, cb) {
         cb(error);
     });
 };
-
-router.post('/line/callback', function (req, res) {
-    // console.dir(req.body);
-    const code = req.body.code;
-    getAccessToken(code, function (response) {
-        if (response.status === 200) {
-            // console.log('ok: ' + response.data.access_token);
-            res.send('ok: ' + response.data.access_token);
-        } else {
-            // console.log(response);
-            res.send('fail... ');
-        }
-    });
-});
-
-router.get('/line/regist', function (req, res) {
-    res.render('lineregist', {
-        title: '首頁',
-        client_id: client_id,
-        ipport: ip.address() + ':3000',
-        redirect_uri: redirect_uri
-    });
-});
-
-router.post('/line/sendmsg', function (req, res) {
-    // console.dir(req.body);
-    const token = req.body.token;
-    const msg = req.body.msg;
-    sendMsg(token, msg, function(response) {
-        // TODO: fix
-        res.json();
-    });
-});
 
 module.exports = router;
